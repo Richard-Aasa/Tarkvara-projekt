e375cacda6d02da67f7bfd0711c54d435a9168c7
Richard Aasa <richard.aasa@gmail.com>
2016-04-29 22:21:55 +0300

    test openshift

--- public/controllers/question.js
+++ public/controllers/question.js
@@ -10,22 +10,49 @@
             // NB! Kõik kliendipoolsed addressid mis ei "lae" uut vaadet algavad localhost:3000/#/kamarajura
             // Niiet /#/questions/:id on AngularJS route ja /questions on otseviide serverile andmete saamiseks
             $http.get('/questions').success(function(response) {
-              // Response on momendil objektide massiiv nimega 'questions'
-              // mis on kajastatud failis server.js
+                // Response on momendil objektide massiiv nimega 'questions'
+                // mis on kajastatud failis server.js
 
-              // Minnes addressile /#/questions/1, küsib see kontroller
-              // serverilt vastust addressilt /questions. Server vastab
-              // ja saadab tagasi objektide massiivi. success() meetod
-              // võtab vastuse ja see mida server saatis on callback funktsioonis
-              // 'response' parameetris.
+                // Minnes addressile /#/questions/1, küsib see kontroller
+                // serverilt vastust addressilt /questions. Server vastab
+                // ja saadab tagasi objektide massiivi. success() meetod
+                // võtab vastuse ja see mida server saatis on callback funktsioonis
+                // 'response' parameetris.
 
-              // Lõpuks ütleme, et kui kontroller on laetud, teeme
-              // muutuja "questions" avalikuks ja anname sellele serveri vastuse väärtuseks.
-              // Kui me seda ei tee, ei saa me /public/views/question.html failis kasutada ng-repeat="x in questions"
-              // attribuuti, kuna "questions" ei oleks defineeritud
-              $scope.questions = response;
+                // Lõpuks ütleme, et kui kontroller on laetud, teeme
+                // muutuja "questions" avalikuks ja anname sellele serveri vastuse väärtuseks.
+                // Kui me seda ei tee, ei saa me /public/views/question.html failis kasutada ng-repeat="x in questions"
+                // attribuuti, kuna "questions" ei oleks defineeritud
+                $scope.questions = response;
             });
-
+            $http.get('/questiontypes').success(function(response) {
+                $scope.types = response;
+            });
+            $scope.question = {};
+            $scope.question.maxPoints = 0;
+            $scope.question.variants = [];
+            $scope.addVariant = function(question,variant) {
+                // Peab kopeerima, muidu viitab eksisteerivale sisendile
+                // NB! C keeles = tähendab koopia tegemist
+                // Angularis = tähendab otseviitamist
+                // ehk näiteks:
+                /*
+                  var a = 5;
+                  var c = a;
+                  c = 10;
+                  console.log(a) - annab vastuse 10
+                */
+                $scope.question.variants.push(angular.copy(variant));
+                $scope.question.maxPoints += variant.points;
+                console.log(question);
+                console.log(variant);
+            }
+            $scope.remVariant = function(question,variant) {
+                $scope.question.maxPoints -= variant.points;
+                $scope.question.variants.splice(question.variants.indexOf(variant), 1);
+                console.log(question);
+                console.log(variant);
+            }
         }]);
 
 }());
--- server.js
+++ server.js
@@ -15,34 +15,49 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get('/questions', function(req, res) {
-  // Siin oleks reaalsuses ühendus MONGOGA
-  var questions = [{
-    question: "Millal sündis X",
-    answer: "X sündis aastal . . .",
-    timeStart: new Date(),
-    timeElapsed: new Date() - this.timeStart
-  },
-  {
-    question: "Millal sündis Y",
-    answer: "Y sündis aastal . . .",
-    timeStart: new Date(),
-    timeElapsed: new Date() - this.timeStart
-  },
-  {
-    question: "Millal sündis Z",
-    answer: "Z sündis aastal . . .",
-    timeStart: new Date(),
-    timeElapsed: new Date() - this.timeStart
-  },
-  {
-    question: "Millal sündis W",
-    answer: "W sündis aastal . . .",
-    timeStart: new Date(),
-    timeElapsed: new Date() - this.timeStart
-  }];
-  res.json(questions);
+    // Siin oleks reaalsuses ühendus MONGOGA
+    var questions = [{
+        title: "Millal sündis X",
+        type: 'Valik',
+        variants: [{
+            answer: "1337",
+            points: 0,
+            bool: false
+        }, {
+            answer: "1990",
+            points: 0,
+            bool: false
+        }, {
+            answer: "2016",
+            points: 15,
+            bool: true
+        }],
+        maxPoints: 15
+    }, {
+        title: "Millal sündis Y",
+        type: 'Sisestus',
+        variants: [{
+            answer: "Aastal 1928",
+            points: 5
+        }, {
+            answer: "Aastal 1928, kuskil Siberis",
+            points: 10
+        }, {
+            answer: "Aastal",
+            points: 15
+        }, {
+            answer: "Kuskil Siberis",
+            points: 1
+        }],
+        maxPoints: 15
+    }];
+    res.json(questions);
 });
-
+app.get('/questiontypes', function(req, res) {
+    // Siin oleks reaalsuses ühendus MONGOGA
+    var types = ["Valik", "Paigutus", "Sisestus"];
+    res.json(types);
+});
 // Mis pordi peal kuulata muudatusi
 app.listen(3000);
 console.log("Hosting on port 3000");
