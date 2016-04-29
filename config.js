1438aed78c377a2e9157afd5faa7397cc1918e1e
Richard Aasa <richard.aasa@gmail.com>
2016-04-30 01:16:51 +0300

    script to start

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb://admin:rZ5IBwq1TkDI@127.0.0.1/test1',
+    db: 'mongodb://admin:rZ5IBwq1TkDI@test1-aasa.rhcloud.com/test1',
     secret: 'big_secret',
     googleKey: '',
     googleSecret: ''
