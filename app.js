31748638b42ecdbb01f3188e16a7f5069c5d58ee
Richard Aasa <richard.aasa@gmail.com>
2016-04-29 23:47:14 +0300

    script to start

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
-var http = require('http'),
-    fs = require('fs'),
-    path = require('path'),
-    contentTypes = require('./utils/content-types'),
-    sysInfo = require('./utils/sys-info'),
-    express = require('express'),
+var express = require('express'),
     env = process.env;
 
-// var server = http.createServer(function(req, res) {
-//     let url = req.url;
-//     if (url == '/') {
-//         url += 'index.html';
-//     }
-//
-//     // IMPORTANT: Your application HAS to respond to GET /health with status 200
-//     //            for OpenShift health monitoring
-//
-//     if (url == '/health') {
-//         res.writeHead(200);
-//         res.end();
-//     } else if (url.indexOf('/info/') == 0) {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.setHeader('Cache-Control', 'no-cache, no-store');
-//         res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-//     } else {
-//         fs.readFile('./static' + url, function(err, data) {
-//             if (err) {
-//                 res.writeHead(404);
-//                 res.end();
-//             } else {
-//                 let ext = path.extname(url).slice(1);
-//                 res.setHeader('Content-Type', contentTypes[ext]);
-//                 if (ext === 'html') {
-//                     res.setHeader('Cache-Control', 'no-cache, no-store');
-//                 }
-//                 res.end(data);
-//             }
-//         });
-//     }
-// });
 var server = express();
 server.use(express.static(__dirname + "/public"));
 
@@ -84,7 +47,7 @@ server.get('/questions', function(req, res) {
     }];
     res.json(questions);
 });
-server.get('/questiontypes', function(req, res) {
+server.get('/types', function(req, res) {
     // Siin oleks reaalsuses ühendus MONGOGA
     var types = ["Valik", "Paigutus", "Sisestus"];
     res.json(types);
