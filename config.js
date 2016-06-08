aa2bab0d9b72208ad7af28c4d54af2c3816019f8
Richard Aasa <richard.aasa@gmail.com>
2016-06-08 15:37:57 +0300

    asdfasdf
    asdasdasd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb://janekos:eeccec@127.0.0.1/test',
+    db: 'mongodb://richard:123@127.0.0.1/test',
     secret: 'big_secret',
     googleKey: '',
     googleSecret: ''
