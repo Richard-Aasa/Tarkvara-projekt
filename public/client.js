6972ce53c76d9525b18bcbbfa855a32114832d97
merit26 <merit.paist@gmail.com>
2016-06-10 11:09:56 +0300

    Revert "my updates"
    This reverts commit 4cd722e47de0be405ac55be5aec685ccd73c7455.

---
+++ public/client.js
@@ -0,0 +1,46 @@
+// See fail tegeleb kliendi poolse routimisega.
+// Kuna tegemist on SPA-ga (Single-Page Application)
+// on vaja loogikat mis muudaks lehe sisu, selleks see fail ongi.
+(function() {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngResource', 'ngMaterial'])
+        .config(['$routeProvider', '$locationProvider', '$resourceProvider',
+            function($routeProvider, $locationProvider, $resourceProvider) {
+
+                $routeProvider
+                    .when('/', {
+                        templateUrl: '/views/home.html',
+                        controller: 'HomeController'
+                    })
+                    //localhost:3000/#/ siia otsa rakendub see .when, et kui url on /test/question/ siis laetakse seda vaadet
+                    .when('/question', {
+                        templateUrl: '/views/question.html',
+                        controller: 'QuestionController'
+                    })
+                    .when('/questionnaire', {
+                        templateUrl: '/views/questionnaire.html',
+                        controller: 'QuestionnaireController'
+                    })
+                    .when('/fill', {
+                        templateUrl: '/views/fill.html',
+                        controller: 'FillController'
+                    })
+                    .when('/login', {
+                        templateUrl: '/views/login.html',
+                        controller: 'QuestionController'
+                    })
+                    .when('/register', {
+                        templateUrl: '/views/register.html',
+                        controller: 'QuestionController'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+
+                //testing
+
+            }
+        ]);
+}());
