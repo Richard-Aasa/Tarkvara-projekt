f3ad1e365ca623810a511f5680ece77ce415bb39
Richard Aasa <richard.aasa@gmail.com>
2016-06-14 18:15:50 +0300

    Mhm

--- public/controllers/stat.js
+++ public/controllers/stat.js
@@ -7,12 +7,12 @@
 
             $scope.loading = true;
             $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+              [65, 59, 80, 81, 56, 55, 40],
+              [28, 48, 40, 19, 86, 27, 90]
+            ];
+
 
 
             // WORKS
