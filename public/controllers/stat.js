f36e2db140638023c351b7984ef2c69a8d278c3b
naaber <martti.naaber@gmail.com>
2016-06-15 10:32:08 +0300

    stat template works

--- public/controllers/stat.js
+++ public/controllers/stat.js
@@ -17,15 +17,6 @@
         })
         .controller('StatController', ['$scope', '$mdDialog', function($scope, $mdDialog) {
 
-            $scope.loading = true;
-            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-            $scope.series = ['Series A', 'Series B'];
-            $scope.data = [
-              [65, 59, 80, 81, 56, 55, 40],
-              [28, 48, 40, 19, 86, 27, 90]
-            ];
-
-
             $scope.loading = false;
 
             // WORKS
@@ -41,7 +32,6 @@
             //             console.log(error);
             //         }
             //     );
-
             //näiteandmed
             $scope.questionnaires = [{
                 questionnaire: 1,
@@ -64,20 +54,40 @@
                 userPoints: 40
             }];
 
+
             $scope.chartOptions = {
+
+                chart: {
+                    type: 'bar',
+                },
+
                 title: {
-                    text: $scope.questionnaires[0].questionnaire
+                    text: "asd"
                 },
                 xAxis: {
                     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dets'
                     ]
+                },
+
+                yAxis: {
+                    allowDecimals: false,
+                    title: {
+                        text: null
+                    },
+                    min: 0,
+                    max: 10
                 },
 
                 series: [{
                     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
                 }]
             };
+
+
+
+
+
 
         }]);
 }());
