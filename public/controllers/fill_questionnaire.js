a5fda7938c8af5af4765404e7f31e0877a6f4b2c
Janek Kossinski <janek.kossinski@gmail.com>
2016-06-15 09:45:33 +0300

    fill_questionnaire.js controller
    started fill_questionnaire.js controller to view fill_questionnaire.html

---
+++ public/controllers/fill_questionnaire.js
@@ -0,0 +1,9 @@
+(function() {
+	'use strict';
+	
+	angular
+        .module('app')
+        .controller('FillQuestionnaireController', ['$scope','AuthenticateService','$interval', '$mdDialog', function($scope, AuthenticateService, $interval, $mdDialog) {
+			
+		}]);
+}());
