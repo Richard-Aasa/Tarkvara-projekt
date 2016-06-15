7fe1d42141574c8879e6674f69c860e995531f38
naaber <martti.naaber@gmail.com>
2016-06-15 09:18:27 +0300

    stat

--- public/controllers/stat.js
+++ public/controllers/stat.js
@@ -66,7 +66,7 @@
 
             $scope.chartOptions = {
                 title: {
-                    text: ""
+                    text: $scope.questionnaires[0].questionnaire
                 },
                 xAxis: {
                     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
@@ -78,10 +78,6 @@
                     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
                 }]
             };
-
-
-
-
 
         }]);
 }());
