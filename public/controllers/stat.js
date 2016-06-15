f2f7ff36f48ebce3ebc597ab61fddd97b021a670
naaber <martti.naaber@gmail.com>
2016-06-15 15:59:13 +0300

    new chart

--- public/controllers/stat.js
+++ public/controllers/stat.js
@@ -50,8 +50,7 @@
                     totalTime: 1,
                     points: 1,
                     correct: true
-                },
-                ],
+                }, ],
                 userTime: 60,
                 userPoints: 40
             }, {
@@ -174,7 +173,7 @@
             };
 
             //diagramm, mis kuvab kõikide kasutajate punktid, mis nad said terve küsimustiku eest
-            $scope.chartPoints = {
+            $scope.chartUserPoints = {
                 chart: {
                     type: 'bar'
                 },
@@ -197,7 +196,8 @@
                 }]
             };
 
-            $scope.chartTime = {
+            //diagramm, mis näitab iga kasutaja puhul, kui palju aega kulus tal iga küsimuse peale
+            $scope.chartUserTime = {
                 chart: {
                     type: 'bar'
                 },
@@ -222,6 +222,81 @@
                     }
                 },
                 series: $scope.addResultsChartTwo($scope.statistics)
+            };
+
+            $scope.chartQuestionTime = {
+
+                chart: {
+                    type: 'boxplot'
+                },
+
+                title: {
+                    text: 'Highcharts Box Plot Example'
+                },
+
+                legend: {
+                    enabled: false
+                },
+
+                xAxis: {
+                    categories: ['1', '2', '3', '4', '5'],
+                    title: {
+                        text: 'Experiment No.'
+                    }
+                },
+
+                yAxis: {
+                    title: {
+                        text: 'Observations'
+                    },
+                    plotLines: [{
+                        value: 932,
+                        color: 'red',
+                        width: 1,
+                        label: {
+                            text: 'Theoretical mean: 932',
+                            align: 'center',
+                            style: {
+                                color: 'gray'
+                            }
+                        }
+                    }]
+                },
+
+                series: [{
+                    name: 'Observations',
+                    data: [
+                        [760, 801, 848, 895, 965],
+                        [733, 853, 939, 980, 1080],
+                        [714, 762, 817, 870, 918],
+                        [724, 802, 806, 871, 950],
+                        [834, 836, 864, 882, 910]
+                    ],
+                    tooltip: {
+                        headerFormat: '<em>Experiment No {point.key}</em><br/>'
+                    }
+                }, {
+                    name: 'Outlier',
+                    color: Highcharts.getOptions().colors[0],
+                    type: 'scatter',
+                    data: [ // x, y positions where 0 is the first category
+                        [0, 644],
+                        [4, 718],
+                        [4, 951],
+                        [4, 969]
+                    ],
+                    marker: {
+                        fillColor: 'white',
+                        lineWidth: 1,
+                        lineColor: Highcharts.getOptions().colors[0]
+                    },
+                    tooltip: {
+                        pointFormat: 'Observation: {point.y}'
+                    }
+                }]
+
+
+
             };
 
         }]);
